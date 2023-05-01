0c6d2de15a95d0972370a816144b7b386b7202d2
christinakerying <93345258+christinakerying@users.noreply.github.com>
2023-04-30 20:59:26 -0700

    Create part2-question19.js

---
+++ expose/javascript/part2-question19.js
@@ -0,0 +1,8 @@
+function printNums() {
+  console.log(1);
+  setTImeout(function() { console.log(2); }, 
+  setTimeout(function() { console.log(3); },
+  console.log(4);
+}
+             
+             printNums();
