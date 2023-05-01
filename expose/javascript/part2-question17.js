01ae2cd5f3d4e083eef35ea43e97790f72912c4a
christinakerying <93345258+christinakerying@users.noreply.github.com>
2023-04-30 20:30:04 -0700

    Create part2-question17.js

---
+++ expose/javascript/part2-question17.js
@@ -0,0 +1,13 @@
+functio nmodifyArray(array,callback) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(callback(array[i]));
+  }
+  return newArr;
+}
+
+function doSomething(num) {
+  return num*2;
+}
+
+modifyArray([1, 2, 3], doSomething);
